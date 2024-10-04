933133f4675ae907183318a1a1dac80e8d5614c0
AnastasiyaShvydko <84057111+AnastasiyaShvydko@users.noreply.github.com>
2024-10-04 10:30:44 -0400

    week3

--- group-project-test/ui/all/nav-links.tsx
+++ group-project-test/ui/all/nav-links.tsx
@@ -1,8 +1,8 @@
 'use client';
 
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import clsx from 'clsx';
+//import { usePathname } from 'next/navigation';
+//import clsx from "clsx";
 
 
 const links = [
@@ -20,7 +20,7 @@ const links = [
   ];
 
   export default function NavLinks() {
-    const pathname = usePathname();
+    //const pathname = usePathname();
     return (
       <>
         {links.map((link) => {
@@ -28,12 +28,12 @@ const links = [
             <Link
               key={link.name}
               href={link.href}
-              className={clsx(
-                'flex h-[48px] grow items-center justify-center gap-2 rounded-md dark-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-center md:p-2 md:px-3',
-                {
-                  'bg-sky-100 text-blue-600': pathname === link.href,
-                },
-              )}
+            //   className={clsx(
+            //     'flex h-[48px] grow items-center justify-center gap-2 rounded-md dark-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-center md:p-2 md:px-3',
+            //     {
+            //       'bg-sky-100 text-blue-600': pathname === link.href,
+            //     },
+            //   )}
             >
               {/* <LinkIcon className="w-6" /> */}
               <p className=" md:flex justify-center">{link.name}</p>
